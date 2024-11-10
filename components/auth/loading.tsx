17c66f9115e2a1000496e10d24aeedba5dc38865
uddisharma <uddibhardwaj08@gmail.com>
2024-11-10 21:32:24 +0530

    Skeleton

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,12 +4,15 @@ export const Loading = () => {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
       <Image
-        src="/logo.svg"
+        src="/logo.png"
         alt="Logo"
         width={120}
         height={120}
         className="animate-pulse duration-700"
       />
+      <h2 className="text-2xl font-semibold mt-6 animate-pulse duration-700">
+        Loading...
+      </h2>
     </div>
   );
 };
