1248da4b2315503d82f8f71cf9e3c45c0db391e8
uddisharma <uddibhardwaj08@gmail.com>
2024-11-10 16:06:04 +0530

    app initialize

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,30 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
- 
+// import { authMiddleware } from "@clerk/nextjs";
+
+// export default authMiddleware({
+//   publicRoutes: ["/"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)",
+//     "/dashboard(.*)", "/(marketing)(.*)", "/(public)(.*)", "/(settings)(.*)", "/(user)(.*)"],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+2
+const isPublicRoute = createRouteMatcher(['/dashboard(.*)', '/'])
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+    "/dashboard(.*)", "/(marketing)(.*)", "/(public)(.*)", "/(settings)(.*)", "/(user)(.*)"
+  ],
+}
