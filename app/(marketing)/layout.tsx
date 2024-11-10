8cf889f4aad00f0858371a86f6794da63fca00d4
uddisharma <uddibhardwaj08@gmail.com>
2024-11-10 11:34:36 +0530

    marketing design

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,27 @@
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Navbar } from "@/components/layout/navbar";
+import { ThemeProvider } from "@/components/layout/theme-provider";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <div className={cn("min-h-screen bg-background", inter.className)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+
+          {children}
+        </ThemeProvider>
+      </div>
+  
+  );
+}
