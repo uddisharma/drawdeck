6f5cd6b01f7576b41d9134641e8e5fc8f065c1ea
uddisharma <uddibhardwaj08@gmail.com>
2024-11-14 21:42:13 +0530

    preview added

---
+++ app/(preview)/preview/[boardId]/page.tsx
@@ -0,0 +1,17 @@
+import { Canvas } from "../_components/canvas";
+
+ interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
+  return (
+    <div >
+      <Canvas boardId={params.boardId} />
+    </div>
+  );
+};
+
+export default BoardIdPage;
