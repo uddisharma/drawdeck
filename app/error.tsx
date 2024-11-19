c32cc66819b5fa12c0ffbfb1907be80e0234133c
uddisharma <uddibhardwaj08@gmail.com>
2024-11-19 21:34:16 +0530

    Error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 
 const Error = () => {
-    return (
-        <div className='flex justify-center align-middle items-center w-[100vw] h-[100vh]'>
-            <h1>Something went wrong !</h1>
-            <p>please try again in some time</p>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center align-middle items-center w-[100vw] h-[100vh] text-center text-gray-200">
+      <div>
+        <h1 className="text-2xl opacity-60 hover:opacity-100">Something went wrong !</h1>
+        <p className="mt-2 opacity-60 hover:opacity-100">please try again in some time</p>
+      </div>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
+
